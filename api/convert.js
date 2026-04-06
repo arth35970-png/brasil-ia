8019bfa0e715b14ab10e15c13f6ce4a9133f360a
arth35970-png <arth35970@gmail.com>
2026-04-06 11:31:32 -0300

    Implement file conversion handler in convert.js

---
+++ api/convert.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const API_KEY = "4719ae74f413d999fb2a28b6b277ea0f";
+
+  const body = JSON.parse(req.body);
+
+  const response = await fetch("https://api.convertio.co/convert", {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + API_KEY,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      input: "base64",
+      file: body.file,
+      filename: body.filename,
+      outputformat: "mp3"
+    })
+  });
+
+  const data = await response.json();
+
+  res.status(200).json({ id: data.data.id });
+}
