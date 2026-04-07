c15db54866c3be64b3ce08c5b382fa10f3acf145
arth35970-png <arth35970@gmail.com>
2026-04-07 20:06:42 -0300

    Create status.js

---
+++ pages/api/status.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  try {
+    const API_KEY = process.env.CONVERTIO_API_KEY;
+    const { id } = req.query;
+
+    if (!id) return res.status(400).json({ error: "ID não enviado" });
+
+    const response = await fetch(`https://api.convertio.co/convert/${id}/status`, {
+      headers: { "Authorization": "Bearer " + API_KEY }
+    });
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
