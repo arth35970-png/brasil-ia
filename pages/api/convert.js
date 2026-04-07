16f7b574a0fe70db73ca37ebeec5eb642cd538ee
arth35970-png <arth35970@gmail.com>
2026-04-07 20:11:23 -0300

    Update convert.js

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -22,7 +22,6 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-
     if (!data.data?.id) {
       return res.status(500).json({ error: "Falha ao iniciar conversão", detalhes: data });
     }
